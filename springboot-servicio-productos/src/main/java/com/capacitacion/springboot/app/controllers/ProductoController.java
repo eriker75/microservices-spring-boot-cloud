611d80bdd54f830e124bac3d0bf0293043288053
Jhony Oliveira <joliveira@sms-latam.com>
2019-08-13 14:00:59 -0300

    Se obtiene puerto desde notación Spring @Value en vez de Environment

--- springboot-servicio-productos/src/main/java/com/capacitacion/springboot/app/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/capacitacion/springboot/app/controllers/ProductoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,17 +15,17 @@ import com.capacitacion.springboot.app.productos.models.service.IProductoService
 
 @RestController
 public class ProductoController {
-
-	@Autowired
-	private Environment env;
+		
+	@Value("${server.port}")
+	private Integer port;
 	
 	@Autowired
 	private IProductoService productoService;
 	
 	@GetMapping("/listar")
 	public List<Producto> listar() {
-		return productoService.findAll().stream().map(producto ->{
-			producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		return productoService.findAll().stream().map(producto ->{	
+			producto.setPort(port);
 			return producto;
 		}).collect(Collectors.toList());
 	}
@@ -33,7 +34,7 @@ public class ProductoController {
 	public Producto detalle(@PathVariable Long id) {
 		
 		Producto producto = productoService.findById(id);
-		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));				
+		producto.setPort(port);
 		return producto;
 	}
 }
