dfa40336a40a55761e399eb268a934eb63d06fc8
Jhony Oliveira <joliveira@sms-latam.com>
2019-08-30 17:08:28 -0300

    Agrega import.sql para que se pueda inyectar de manera automatica al levantar la aplicación

---
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('smsdev1', '12345', 1, 'Jhony', 'Vidal', 'joliveira@sms-latam.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('smsdev2', '12345', 1, 'JV94848846', 'AD', 'smsdev2@sms-latam.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '12345', 1, 'Admin', 'Admin', 'admin@sms-latam.com');
+
