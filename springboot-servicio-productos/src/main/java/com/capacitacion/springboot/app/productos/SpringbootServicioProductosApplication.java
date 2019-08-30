898175efbae47240f28ad6f303e05f06192fad15
Jhony Oliveira <joliveira@sms-latam.com>
2019-08-30 13:40:49 -0300

    Agrega Escaneo de dependencias en microservicio de Productos para que se escanea el módulo Entity de Productos desde el microservicio Commons

--- springboot-servicio-productos/src/main/java/com/capacitacion/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ springboot-servicio-productos/src/main/java/com/capacitacion/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -2,10 +2,12 @@ package com.capacitacion.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com.capacitacion.springboot.app"})
+@EntityScan({"com.capacitacion.springboot.app.commons.modes.entity"})
 public class SpringbootServicioProductosApplication {
 
 	public static void main(String[] args) {
