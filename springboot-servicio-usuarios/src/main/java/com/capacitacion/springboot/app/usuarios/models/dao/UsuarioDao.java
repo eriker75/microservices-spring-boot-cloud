43ab60cada54996a23958590564d76265f0dee5f
Jhony Oliveira <joliveira@sms-latam.com>
2019-08-31 10:50:38 -0300

    Organiza dependencias y Agrega EntityScan en servicio-usuarios SpringBootApplication apuntando a servicios-usuarios-commons

--- springboot-servicio-usuarios/src/main/java/com/capacitacion/springboot/app/usuarios/models/dao/UsuarioDao.java
+++ springboot-servicio-usuarios/src/main/java/com/capacitacion/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-import com.capacitacion.springboot.app.usuarios.models.entity.Usuario;
+import com.capacitacion.springboot.app.usuarios.commons.models.entity.Usuario;
 
 /**
  * @author joliveira
