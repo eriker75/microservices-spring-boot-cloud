91fa54445ba6012409db72c3eb777bc2d16df9f2
Jhony Oliveira <joliveira@sms-latam.com>
2019-08-31 10:10:34 -0300

    Expone Ids en RepositoryRest con RepositoryConfig

---
+++ springboot-servicio-usuarios/src/main/java/com/capacitacion/springboot/app/usuarios/RepositoryConfig.java
@@ -0,0 +1,18 @@
+package com.capacitacion.springboot.app.usuarios;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import com.capacitacion.springboot.app.usuarios.models.entity.Role;
+import com.capacitacion.springboot.app.usuarios.models.entity.Usuario;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer{
+
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Usuario.class, Role.class);
+	}
+	
+		
+}
