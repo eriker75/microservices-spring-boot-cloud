585b705487b18898781f99d8747e9dd6a839f50b
Jhony Oliveira <joliveira@sms-latam.com>
2019-08-31 16:11:18 -0300

    Agrega Log y lanza execepción en el caso de que el usuario es nulo

--- springboot-servicio-oauth/src/main/java/com/capacitacion/springboot/app/oauth/services/UsuarioService.java
+++ springboot-servicio-oauth/src/main/java/com/capacitacion/springboot/app/oauth/services/UsuarioService.java
@@ -3,6 +3,8 @@ package com.capacitacion.springboot.app.oauth.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -16,11 +18,19 @@ import com.capacitacion.springboot.app.usuarios.commons.models.entity.Usuario;
 
 public class UsuarioService implements UserDetailsService{
 
+	private Logger log = LoggerFactory.getLogger(UsuarioService.class);
+	
 	@Autowired
 	private UsuarioFeignClient client;
 	
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		Usuario usuario = client.findByUsername(username);
+		
+		if(usuario == null) {
+			log.error("Error en el login, no existe el usuario '" + username + "' en el sistema");
+			throw new UsernameNotFoundException("Error en el login, no existe el usuario '" + username + "' en el sistema");
+			
+		}
 		
 		// Lista de Roles en Spring Security es del tipo GrantedAuthority
 		List<GrantedAuthority> authorities = usuario.getRoles()
