08148eb75cdfdfcde1a2068268c6f37091f0034f
Jhony Oliveira <joliveira@sms-latam.com>
2019-09-03 16:28:27 -0300

    Agrega metodo para que encripte las contraseñas al arrancar servicio OAuth

--- springboot-servicio-oauth/src/main/java/com/capacitacion/springboot/app/oauth/SpringbootServicioOauthApplication.java
+++ springboot-servicio-oauth/src/main/java/com/capacitacion/springboot/app/oauth/SpringbootServicioOauthApplication.java
@@ -1,17 +1,36 @@
 package com.capacitacion.springboot.app.oauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
-public class SpringbootServicioOauthApplication {
+public class SpringbootServicioOauthApplication implements CommandLineRunner{
 
+	@Autowired
+	private BCryptPasswordEncoder passwordEncode;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootServicioOauthApplication.class, args);
 	}
 
+	/* 
+	 * Metodo encargado de ejecutar una tarea automatizada a nivel linea de comando
+	 */
+	@Override
+	public void run(String... args) throws Exception {
+	
+		String password = "12345";
+		
+		for (int i = 0; i < 4; i++) {
+			String passwordBCrypt = passwordEncode.encode(password);
+			System.out.print(passwordBCrypt);
+		}		
+	}
 }
--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('smsdev1', '12345', 1, 'Jhony', 'Vidal', 'joliveira@sms-latam.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('smsdev2', '12345', 1, 'JV94848846', 'AD', 'smsdev2@sms-latam.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '12345', 1, 'Admin', 'Admin', 'admin@sms-latam.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('smsdev1', '$2a$10$n8vTQ3OJpuN9aNkfaL3DS.CWI8ww0ypgl5qR0OKUUdbK3.TyYBr0q', 1, 'Jhony', 'Vidal', 'joliveira@sms-latam.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('smsdev2', '$2a$10$mqVkLqkq0UXXONLRwd3VJ.QfvhvJzM7vLfI3o.3aWA3kPU/NnhtUK', 1, 'JV94848846', 'AD', 'smsdev2@sms-latam.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$Q0KuCMLGKHe.0zm9GpCsu.HYZT23Wo4B9kqxElWEmjZQJRh7S.o0W', 1, 'Admin', 'Admin', 'admin@sms-latam.com');
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USE');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
