4eab16918d2a22c75a7d1ebe0daba4effad30050
Jhony Oliveira <joliveira@sms-latam.com>
2019-09-18 11:05:11 -0300

    Agrega dependencias y configuraciones para que el micro servicio utilice PostgreSQL

--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,6 +1,8 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('smsdev1', '$2a$10$n8vTQ3OJpuN9aNkfaL3DS.CWI8ww0ypgl5qR0OKUUdbK3.TyYBr0q', 1, 'Jhony', 'Vidal', 'joliveira@sms-latam.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('smsdev2', '$2a$10$mqVkLqkq0UXXONLRwd3VJ.QfvhvJzM7vLfI3o.3aWA3kPU/NnhtUK', 1, 'JV94848846', 'AD', 'smsdev2@sms-latam.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$Q0KuCMLGKHe.0zm9GpCsu.HYZT23Wo4B9kqxElWEmjZQJRh7S.o0W', 1, 'Admin', 'Admin', 'admin@sms-latam.com');
+--- Estamos usando PostgreSQL por lo que tenemos que quitar las comillas simples en el nombre de la tabla que insertamos
+--- También los datos booleanos de la tabla usuarios directamente se cargan como true o falso
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('smsdev1', '$2a$10$n8vTQ3OJpuN9aNkfaL3DS.CWI8ww0ypgl5qR0OKUUdbK3.TyYBr0q', true, 'Jhony', 'Vidal', 'joliveira@sms-latam.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('smsdev2', '$2a$10$mqVkLqkq0UXXONLRwd3VJ.QfvhvJzM7vLfI3o.3aWA3kPU/NnhtUK', true, 'JV94848846', 'AD', 'smsdev2@sms-latam.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$Q0KuCMLGKHe.0zm9GpCsu.HYZT23Wo4B9kqxElWEmjZQJRh7S.o0W', true, 'Admin', 'Admin', 'admin@sms-latam.com');
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USE');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
